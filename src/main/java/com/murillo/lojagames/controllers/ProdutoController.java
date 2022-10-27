41112b8a5b6335374863c71a1ad7fbe59a05728b
MurilloNS <murillo.murillo2001@gmail.com>
2022-10-27 11:31:34 -0300

    Adding Images in ProductController

--- src/main/java/com/murillo/lojagames/controllers/ProdutoController.java
+++ src/main/java/com/murillo/lojagames/controllers/ProdutoController.java
@@ -1,9 +1,16 @@
 package com.murillo.lojagames.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.murillo.lojagames.entities.Produto;
@@ -23,8 +30,23 @@ public class ProdutoController {
 	}
 
 	@PostMapping("/produto/salvar")
-	public ModelAndView salvar(Produto produto) {
+	public ModelAndView salvar(Produto produto, @RequestParam("file") MultipartFile arquivo) {
 		produtoService.salvar(produto);
+
+		try {
+			if (!arquivo.isEmpty()) {
+				byte[] bytes = arquivo.getBytes();
+				Path caminho = Paths
+						.get(caminhoImagens + String.valueOf(produto.getId()) + arquivo.getOriginalFilename());
+				Files.write(caminho, bytes);
+
+				produto.setImagem(String.valueOf(produto.getId()) + arquivo.getOriginalFilename());
+				produtoService.salvar(produto);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return cadastrar(new Produto());
 	}
 }
